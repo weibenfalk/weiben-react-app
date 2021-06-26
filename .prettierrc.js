88eb8d05ec7e9846e05acffe9ce59aafe2cc94a3
Thomas Weibenfalk <thomas.weibenfalk@gmail.com>
2021-06-26 19:26:40 +0200

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  jsxBracketSameLine: true,
+  endOfLine: 'auto',
+  arrowParens: "avoid",
+};
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <p>This is my current React setup for my projects.</p>
+      <p>For free videos on learning development visit and subscribe to my channel:</p>
+      <a href="http://youtube.com/weibenfalk">youtube.com/weibenfalk</a>
+    </div>
+  );
+};
+
+export default App;
